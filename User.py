b3c6f453eb5f8e65ce9119016d2ac9020e0245cd
zainab_bouaziz <135221435+ZainabBouaziz@users.noreply.github.com>
2024-01-27 20:11:23 +0100

    Update User.py

--- User.py
+++ User.py
@@ -2,39 +2,35 @@ from Bank_Account import Checking_Account,Retirement_Account,BankAccount
 
 class User:
     
-    def __init__(self,name,email):
+    def __init__(self,name,email,account):
         self.name=name
         self.email=email
-        self.account={
-            'checking':Checking_Account(int_rate=0.2,balance=0),
-            'Retirement':Retirement_Account(int_rate=0.2,is_roth=False,balance=0),
-        }
+        self.account=account
 
-    def make_deposit(self,amount,account):
-        self.account[account].deposit(amount)
+    def make_deposit(self,amount):
+        self.account.deposit(amount)
         return self
 
-    def make_withdrawal(self,amount,account):
-        self.account[account].withdraw(amount)
+    def make_withdrawal(self,amount):
+        self.account.withdraw(amount)
         return self
     
-    def display_user_balance(self,account):
+    def display_user_balance(self):
         print('User:',self.name)
-        print(account)
-        self.account[account].display_account_info()
+        self.account.display_account_info()
         return self
 
-    def transfer_money(self, other_user, amount,account_1,account_2):
-        self.account[account_1].withdraw(amount)
-        other_user.account[account_2].deposit(amount)
+    def transfer_money(self, other_user, amount):
+        self.account.withdraw(amount)
+        other_user.account.deposit(amount)
         return self
     
-""" 
-zainab=User('zainab','zainab.bouaziz@gmail.com')
-merya=User('merya','merya.bouaziz@gmail.com')
-
-zainab.make_deposit(50,'checking').display_user_balance('checking').make_deposit(20,'Retirement').display_user_balance('Retirement').transfer_money(merya,50,'checking','Retirement').display_user_balance('checking')
-merya.display_user_balance('Retirement')
+RBA_1=Retirement_Account(0.1,0)
+CBA_1=Checking_Account(0.2,0)
+zainab=User('zainab','zainab.bouaziz@gmail.com',RBA_1)
+merya=User('merya','merya.bouaziz@gmail.com',CBA_1)
+zainab.make_deposit(50).display_user_balance().make_deposit(20).display_user_balance().transfer_money(merya,50).display_user_balance()
+merya.display_user_balance()
 
 
 """
